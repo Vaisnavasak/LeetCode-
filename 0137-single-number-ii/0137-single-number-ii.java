429bad6cf6ee95e8cf7c17b64e8a51d5967bb83a
Vaisnava Sakthivel <vaisnavasakthivels@gmail.com>
2026-07-27 15:41:03 +0530

    Time: 92 ms (5.67%), Space: 45.3 MB (63.24%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public int singleNumber(int[] arr) 
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            int count = 0;
+            for(int j=0;j<arr.length;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count+=1;
+                }
+            }
+            if(count == 1)
+            {
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+}
