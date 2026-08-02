4cbf9dc8788b91432777bd2050608f4024f80950
Vaisnava Sakthivel <vaisnavasakthivels@gmail.com>
2026-08-03 00:37:23 +0530

    Time: 1 ms (99.95%), Space: 94.7 MB (15.82%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        int mini = prices[0];
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++)
+        {
+            if (prices[i] < mini)
+            {
+                mini = prices[i];
+            }
+            else if (prices[i] - mini > profit)
+            {
+                profit = prices[i] - mini;
+            }
+        }
+        return profit;
+    }
+}
